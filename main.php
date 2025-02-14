7d039e31bb093b3b9a02112f0b302d2d43bea44b
Tal Halnanny <lecloveste70@gmail.com>
2025-02-14 14:57:25 +0200

    add  child class chasis + add var safe_to_use to main class car

--- main.php
+++ main.php
@@ -6,14 +6,16 @@ class Car
     var $engine_volume;
     var $year;
     var $safety_rating;
+    var $safe_to_use;
 
-    function __construct($brand, $model, $engine_volume, $year, $safety_rating)
+    function __construct($brand, $model, $engine_volume, $year, $safety_rating,$safe_to_ride)
     {
         $this->brand = $brand;
         $this->model = $model;
         $this->engine_volume = $engine_volume;
         $this->year = $year;
         $this->safety_rating = $safety_rating;
+        $this->safe_to_use = $safe_to_ride;
     }
 
     function get_brand()
@@ -57,4 +59,58 @@ class Car
     {
         $this->brand = $safety_rating;
     }
+
+    function check_rating()
+    {
+        if ($this->safety_rating==0) {
+            $this->safe_to_use = False; }
+  
+    }
+}
+
+
+
+class chasis extends car
+{
+var $id;
+var $total_flaws;
+
+function __construct($id,$brand,$model,$date)
+{
+  $this->brand = $brand;
+  $this->model = $model;
+  $this->id = $id;  
+  $this->total_flaws =  $date;
+}
+
+function set_make_date($tflaws) {
+    $this->total_flaws = $tflaws;
+}
+
+function set_id($id){
+    $this->id = $id;
+}
+
+function get_id()
+{
+    return $this->id; 
+}
+
+function get_make_date() {
+    return $this->total_flaws;
+}
+
+function check_flaws() {
+    if ($this->total_flaws > 3){
+        echo("Chasis is Not Oporational");
+        $this->safety_rating = 0 ;
+        $this->safe_to_use = False;
+    }
+    else{
+        echo("Chasis is Oporational");
+        $this->safe_to_use = True;
+    }
+
+}
+
 }
