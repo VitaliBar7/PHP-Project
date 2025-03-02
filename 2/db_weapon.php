ca8ebb0ed24efcfafd7b8fc184a514ffe71ef8bc
VitaliBar7 <vitalibar7@gmail.com>
2025-03-02 22:19:35 +0200

    FINAL

--- 2/db_weapon.php
+++ 2/db_weapon.php
@@ -1,26 +1,57 @@
 <?php
-$con = new mysqli("localhost", "root", "", "db_weapon");
-if (!$con) {
-    die("Could not connect" . $con->connect_error);
-} else {
-    print("Connect is success");
-    print("<br><br>");
-    print("<hr>");
-    /*
-    $result = $con->query("SELECT * FROM weapons");
-    while ($row = $result->fetch_object()) {
-        print_r($row);
-        print("<hr>");
-    }
-        */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$name_weapon = $_POST['name_weapon'];
-$damage = $_POST['damage_weapon'];
-$rarity = $_POST['rarity_weapon'];
-$type_id = $_POST['type_id_weapon'];
+$conn->query("INSERT INTO weapons (id, name, damage, rarity, type_id) VALUES
+    (1, 'Sword of Doom', 50, 'Legendary', 1),
+    (2, 'Fire Bow', 35, 'Epic', 2),
+    (3, 'Shadow Dagger', 25, 'Rare', 3),
+    (4, 'Lightning Staff', 40, 'Legendary', 4),
+    (5, 'Frost Axe', 30, 'Epic', 5)");
 
-$sql_weapon = "INSERT INTO weapons (name, damage, rarity, type_id) VALUES ('$name', '$damage', '$rarity', '$type_id')";
-if ($con->query($sql_weapon)) {
-    echo ("Weapon added successfully!");
+$conn->query("INSERT INTO weapon_skins (id, skin_name, weapon_id) VALUES
+    (1, 'Golden Blade', 1),
+    (2, 'Flaming Arrows', 2),
+    (3, 'Dark Essence', 3),
+    (4, 'Electric Surge', 4),
+    (5, 'Icy Edge', 5)");
+
+$conn->query("INSERT INTO weapon_upgrades (id, upgrade_name, bonus_damage, bonus_speed, weapon_id) VALUES
+    (1, 'Sharp Edge', 10, 1.2, 1),
+    (2, 'Fire Enhancement', 8, 1.1, 2),
+    (3, 'Poison Boost', 5, 1.5, 3),
+    (4, 'Lightning Charge', 12, 1.3, 4),
+    (5, 'Ice Enhancement', 9, 1.2, 5)");
+
+$conn->query("INSERT INTO weapon_abilities (id, ability_name, cooldown_seconds, effect_description, weapon_id) VALUES
+    (1, 'Power Slash', 5, 'Delivers a strong slash dealing AoE damage', 1),
+    (2, 'Fire Rain', 7, 'Shoots multiple flaming arrows in the air', 2),
+    (3, 'Shadow Step', 3, 'Teleports the user behind the enemy', 3),
+    (4, 'Storm Call', 6, 'Summons a thunderstorm to damage enemies', 4),
+    (5, 'Frozen Blast', 8, 'Unleashes a cold explosion slowing enemies', 5)");
+
+$result = $conn->query("SELECT * FROM weapons");
+while ($row = $result->fetch_assoc()) {
+    echo "Weapon: " . $row['name'] . " - Damage: " . $row['damage'] . "<br>";
 }
+
+$conn->query("DELETE FROM weapon_skins WHERE id = 1");
+$conn->query("DELETE FROM weapon_abilities WHERE id = 3");
+
+$conn->query("UPDATE weapons SET damage = 55 WHERE id = 1");
+$conn->query("UPDATE weapon_upgrades SET bonus_damage = 15 WHERE id = 2");
+
+$conn->close();
+
+/*
+DELETE FROM table_name;
+ALTER TABLE table_name AUTO_INCREMENT = 1;
+*/
---
+++ 2/db_weapon_form.php
@@ -0,0 +1,81 @@
+<?php
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_weapon";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+if (isset($_POST['name']) && isset($_POST['damage']) && isset($_POST['rarity']) && isset($_POST['type_id'])) {
+    $name = $_POST['name'];
+    $damage = $_POST['damage'];
+    $rarity = $_POST['rarity'];
+    $type_id = $_POST['type_id'];
+
+    $sql = "INSERT INTO weapons (name, damage, rarity, type_id) VALUES ('$name', '$damage', '$rarity', '$type_id')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New weapon inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+if (isset($_POST['skin_name']) && isset($_POST['weapon_id'])) {
+    $skin_name = $_POST['skin_name'];
+    $weapon_id = $_POST['weapon_id'];
+
+    $sql = "INSERT INTO weapon_skins (skin_name, weapon_id) VALUES ('$skin_name', '$weapon_id')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New skin inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+if (isset($_POST['upgrade_name']) && isset($_POST['bonus_damage']) && isset($_POST['bonus_speed']) && isset($_POST['weapon_id'])) {
+    $upgrade_name = $_POST['upgrade_name'];
+    $bonus_damage = $_POST['bonus_damage'];
+    $bonus_speed = $_POST['bonus_speed'];
+    $weapon_id = $_POST['weapon_id'];
+
+    $sql = "INSERT INTO weapon_upgrades (upgrade_name, bonus_damage, bonus_speed, weapon_id) VALUES ('$upgrade_name', '$bonus_damage', '$bonus_speed', '$weapon_id')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New upgrade inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    if (isset($_POST['ability_name']) && isset($_POST['cooldown_seconds']) && isset($_POST['effect_description']) && isset($_POST['weapon_id'])) {
+        $ability_name = $_POST['ability_name'];
+        $cooldown_seconds = $_POST['cooldown_seconds'];
+        $effect_description = $_POST['effect_description'];
+        $weapon_id = $_POST['weapon_id'];
+
+        $sql = "INSERT INTO weapon_abilities (ability_name, cooldown_seconds, effect_description, weapon_id) VALUES ('$ability_name', '$cooldown_seconds', '$effect_description', '$weapon_id')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New ability inserted successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+
+
+
+
+
+$conn->close();
